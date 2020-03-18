6c3a04d22a316373ad7a82453ede45a90982c8c8
George Taray <Gtaray@Hotmail.com>
2020-03-18 12:30:35 -0700

    V1.0.0 Completed.
    Re-did the layout to make the extension play more nicely with other possible extensions. Moved hero coin down next to dying icon so I can make better use of the space in the defense frame.
    
    Added a bonus soak stat that adds on top of whatever you normally soak with armor.
    
    Added a new SOAK stat to support loot and effects adding bonus soak.

---
+++ scripts/armorsoak.lua
@@ -0,0 +1,8 @@
+-- 
+-- Please see the license.html file included with this distribution for 
+-- attribution and copyright information.
+--
+
+function onInit()
+    DataCommon.addStat("soak", "SOAK", "SOAK", "defense.armor.soak.bonus");
+end
--- scripts/manager_soak.lua
+++ scripts/manager_soak.lua
@@ -26,8 +26,17 @@ function handleApplyDamage(msgOOB)
 
     -- Don't set SOAK when healed
     if not bHeal then
-        local nSoak, nOverflow = calculateSoak(rSource, rTarget, msgOOB.sDamage, nDmg)
-        setSoakAmount(rTarget, nSoak, nOverflow);
+        local nDmg, nRemainder = calculateSoak(rSource, rTarget, msgOOB.sDamage, nDmg)
+        local nSoakBonus = ActorManager2.getStat(rTarget, "soak");
+        local _, nSoakMod, nEffectCount = EffectManagerICRPG.getEffectsBonus(rTarget, "SOAK", false);
+        Debug.chat(nSoakMod);
+        if nEffectCount > 0 then
+            nSoakBonus = nSoakBonus + nSoakMod;
+        end
+        local nAdjustedSoak = math.max(nDmg - nSoakBonus, 1);
+
+        -- Subtract soak and soakbonus
+        setSoakAmount(rTarget, nDmg, nRemainder, nAdjustedSoak);
     end
     ActionEffort.handleApplyDamage(msgOOB)
 end
@@ -37,7 +46,6 @@ function calculateSoak(rSource, rTarget, sDesc, nDmg)
     if sTargetType ~= "pc" then return; end
     if not sTargetNode then return; end
     if not nDmg or nDmg < 0 then return; end
-
 
     local aNotifications = {};
     local nTotalHP, nWounds;
@@ -54,13 +62,14 @@ end
 -- but undoing that damage needs to take into account the overflow
 -- EXMAPLE: If a PC is at 8 HP and takes 5 damage, SOAKING can't just heal 5, as that results in
 -- an end total of 5 WOUNDS, not 8.
-function setSoakAmount(rActor, nAmount, nOverflow)
+function setSoakAmount(rActor, nDmg, nRemainder, nSoak)
     local sActorType, nodeActor = ActorManager.getTypeAndNode(rActor);
     if sActorType ~= "pc" then return; end
     if not nodeActor then return; end
-    if not nAmount or nAmount < 0 then return; end
-    DB.setValue(nodeActor, "defense.armor.soak", "number", nAmount);
-    DB.setValue(nodeActor, "defense.armor.overflow", "number", nOverflow);
+    if not nSoak or nSoak < 0 then return; end
+    DB.setValue(nodeActor, "defense.armor.soak.limit", "number", nDmg)
+    DB.setValue(nodeActor, "defense.armor.soak.value", "number", nSoak);
+    DB.setValue(nodeActor, "defense.armor.soak.overflow", "number", nRemainder);
 end
 
 -----------------------------------------------------------------------
@@ -70,39 +79,57 @@ function applySoak(rActor)
     local sActorType, nodeActor = ActorManager.getTypeAndNode(rActor);
     if not nodeActor then return; end
 
-    local nSoak = DB.getValue(nodeActor, "defense.armor.soak", 0);
-    local nOverflow = DB.getValue(nodeActor, "defense.armor.overflow", 0);
+    local nSoak = DB.getValue(nodeActor, "defense.armor.soak.value", 0);
+    local nBonus = ActorManager2.getStat(rActor, "soak");
+    local nOverflow = DB.getValue(nodeActor, "defense.armor.soak.overflow", 0);
+    local nLimit = DB.getValue(nodeActor, "defense.armor.soak.limit", 0);
+
+    local _, nSoakMod, nEffectCount = EffectManagerICRPG.getEffectsBonus(rActor, "SOAK", false);
+    Debug.chat(nSoakMod);
+    if nEffectCount > 0 then
+        nBonus = nBonus + nSoakMod;
+    end
+
+    local nTotalSoak = nSoak + nBonus;
+    -- If there's a limit to the soak amount, then clamp the value by that much.
+    if nLimit > 0 then
+        nTotalSoak = math.min(nTotalSoak, nLimit);
+    end
 
     -- If soak is less than damage, print a message in chat, but don't actually reduce armor
     -- since there's no reason to soak this amount.
-    if nSoak <= nOverflow then
+    if nTotalSoak <= nOverflow then
         ChatManager.SystemMessage("You must soat at least " .. (nOverflow + 1) .. " damage.");
         return;
     else
+        -- Use nSoak and not nTotalSoak because the base value needs to be above 0, not the total
         if nSoak > 0 then
             -- Adjusted SOAK is the amount to heal the PC
-            local nAdjustedSoak = nSoak - nOverflow;
+            local nHealAmount = nTotalSoak - nOverflow;
             local nWounds = DB.getValue(nodeActor, "health.wounds", 0);
             local nArmorDmg = DB.getValue(nodeActor, "defense.armor.damage", 0);
             local nArmorBase = DB.getValue(nodeActor, "defense.armor.base", 0);
             local nArmorLoot = DB.getValue(nodeActor, "defense.armor.loot", 0);
             local nArmorTotal = nArmorBase + nArmorLoot;
+            -- Use nSoak instead of nTotalSoak here because nBonus is not counted
             if nArmorDmg + nSoak > nArmorTotal then
                 ChatManager.SystemMessage("Not enough ARMOR to soak " .. nSoak .. " damage.");
                 return;
             end
 
             -- if the soak amount is greature than our wounds, change it
-            if nAdjustedSoak > nWounds then
-                nAdjustedSoak = nWounds;
+            if nHealAmount > nWounds then
+                nHealAmount = nWounds;
             end
-            if nAdjustedSoak > nArmorTotal then
-                nAdjustedSoak = nArmorTotal;
+            if nHealAmount > nArmorTotal then
+                nHealAmount = nArmorTotal;
             end
-            nWounds = nWounds - nAdjustedSoak;
+            nWounds = nWounds - nHealAmount;
+            -- Don't use TotalSoak here since we only add the base value that was soaked, not the 
+            -- amount added on from bonuses/modifiers/effects
             nArmorDmg = nArmorDmg + nSoak;
 
-            messageSoak(rActor, nAdjustedSoak, false);
+            messageSoak(rActor, nSoak, nTotalSoak, nHealAmount, false);
             
             -- Update health and armor dmg in DB
             DB.setValue(nodeActor, "health.wounds", "number", nWounds);
@@ -111,7 +138,7 @@ function applySoak(rActor)
             -- Update conditions to reflect changes
             updateConditions(rActor);
             -- Reset SOAK
-            setSoakAmount(rActor, 0, 0);
+            setSoakAmount(rActor, 0, 0, 0);
         end
     end
 end
@@ -140,14 +167,16 @@ function updateConditions(rActor)
 	end
 end
 
-function messageSoak(rActor, sTotal, bSecret, sExtra)
+function messageSoak(rActor, nSoak, nTotal, nHeal, bSecret, sExtra)
     if not rActor then
 		return;
     end
     
     local rMessage = ChatManager.createBaseMessage(rActor, nil);
     rMessage.icon = "soak"
-    rMessage.text = "[SOAK: " .. sTotal .. "]";
+    rMessage.text = "[SOAK: " .. nSoak .. "] -> [HEAL: " .. nHeal .. "]";
+    rMessage.dice = {}
+    rMessage.diemodifier = nTotal
 
     Comm.deliverChatMessage(rMessage);
 end
